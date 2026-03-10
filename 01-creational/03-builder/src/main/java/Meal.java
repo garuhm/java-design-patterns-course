ec4e4c74a20a63754e488bbd00b18a10b0b0c5e3
gruhmosque01 <garuhmm@gmail.com>
2026-03-10 12:05:54 -0400

    01-03 builder made

--- 01-creational/03-builder/src/main/java/Meal.java
+++ 01-creational/03-builder/src/main/java/Meal.java
@@ -1,5 +1,26 @@
+import java.util.Objects;
+
 public class Meal {
     private final Burger burger;
     private final Drink drink;
     private final Fries fries;
+
+    public static class Builder{
+        private final Burger burger;
+        private Drink drink;
+        private Fries fries;
+
+        Builder(Burger burger){
+            this.burger = Objects.requireNonNull(burger, "burger must be not null");
+        }
+
+        public Builder drink(Drink drink){
+            this.drink = drink;
+            return this;
+        }
+        public Builder fries(Fries fries){
+            this.fries = fries;
+            return this;
+        }
+    }
 }
